aa1ca213fa3fd19c6f6c8f90a328d72a518b3e4c
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2024-05-31 18:41:03 -0400

    remove duplicate

--- modual/port_question_answer_to_word.py
+++ modual/port_question_answer_to_word.py
@@ -2,21 +2,6 @@ import json
 from docx import Document
 from modual.text_color import text_color
 
-def make_it_word():
-    data: dict = {}
-    with open('./question_data/question.json') as f:
-        data = json.load(f)
-    full_file = ''
-    last_section_known = ''
-    for _, value in data.items():
-        if last_section_known != value['section']:
-            last_section_known = value['section']
-            full_file += f'\n\n {value["section"].upper()} \n\n'
-        new_line = f'#{value["question_index"]} {value["question"]}\n\t{value["answer"]}\n\n'
-        full_file += new_line
-    with open('./text_file.txt', 'w', encoding='utf-8') as f:
-        f.write(full_file)
-
 
 def make_it_word_too(path_to_word):
     data = {}
